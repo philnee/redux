10b95fb109984fdea32668bd2752e16a239d7fae
Phil Needham <philip.needham@advancedcomputersoftware.com>
2015-11-30 15:42:45 +0000

    Refactored tally out of results

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner';
+import Tally from './Tally';
 
 export default React.createClass({
 	mixins: [PureRenderMixin],
-	getPair: function() {
-		return this.props.pair || [];
-	},
-	getVotes: function(entry){
-		if(this.props.tally && this.props.tally.has(entry)){
-			return this.props.tally.get(entry);
-		}
-		return 0;
-	},
 	render: function(){
 		return this.props.winner ?
-		<Winner ref="winner" winner={this.props.winner} /> : 
+		<Winner ref="winner" winner={this.props.winner}/> : 
 		<div className="results">
-			<div className="tally">
-				{this.getPair().map(entry =>
-					<div key={entry} className="entry">
-					<h1>{entry}</h1>
-					<div className="voteCount">
-						{this.getVotes(entry)}
-					</div>
-					</div>
-				)}
-			</div>
+			<Tally ref="tally" tally={this.props.tally}
+							   pair={this.props.pair} />
 			<div className="management">
 				<button ref="next"
 						className="next"
